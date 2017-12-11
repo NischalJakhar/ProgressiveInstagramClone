07dba825e3ba2fb111ee86c555fd56222b508e61
NischalJakhar <nischaljakhar@gmail.com>
2017-12-11 14:07:20 +0530

    ON DEMAND USER CACHE

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -31,17 +31,17 @@ shareImageButton.addEventListener('click', openCreatePostModal);
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
 
 //CACHE ON DEMAND CODE NOT IN USE Currently
-// function onSaveButtonClicked(event) {
-//   console.log('clicked');
-//   if('caches' in window)
-//   {
-//       caches.open('user-requested')
-//           .then(function (cache){
-//             cache.add('https://httpbin.org/get');
-//             cache.add('/src/images/sf-boat.jpg');
-//           });
-//   }
-// }
+function onSaveButtonClicked(event) {
+  console.log('clicked');
+  if('caches' in window)
+  {
+      caches.open('user-requested')
+          .then(function (cache){
+            cache.add('https://httpbin.org/get');
+            cache.add('/src/images/sf-boat.jpg');
+          });
+  }
+}
 
 function createCard() {
   var cardWrapper = document.createElement('div');
